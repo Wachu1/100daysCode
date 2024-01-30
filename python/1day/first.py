99268430b9ff872c746dc448cfd6891da3d004f2
Wachu1 <dominik.wach06@gmail.com>
2024-01-30 01:37:44 +0100

    1 day completed

--- python/1day/first.py
+++ python/1day/first.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("hello world\nhello world")  # create new line
+print("hello" + " " + "Angela")  # konkatenacja stringa
+
+name = input("Jak masz na imię? ")
+print("Witaj " + name + "!")
+
+# cmd + / powoduje utworzenie komontarza z danej linijki kodu
+
+print(len(input("Podaj imię obliczę ile ma liter ")))
+# Funkcja len powodouje zliczenie ilości liter w danym stringu
